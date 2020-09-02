8dec3775de6ae4b996065f303fb081a355b7159d
Laszlo Kadar - Linux <laszlo.kadar@rcdotnet.se>
2020-09-02 21:46:37 +0200

    fixing kubernetes launch, adding the output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath="rcdotnet/udap5microservice:project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udap5microservice\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=projecttosend
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udap5microservice 8088:80
 
